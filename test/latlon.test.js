43a3dae80a450916d435b6e3cd697d32321183ad
vishaltvty <Vishal.Chilka@tivityhealth.com>
2020-10-27 15:18:19 -0700

    Added invalid key test

--- test/latlon.test.js
+++ test/latlon.test.js
@@ -17,6 +17,11 @@ let obj4 = {
     webKey: '8945708976961762'
 }
 
+let obj5 = {
+    address: '155 E Frye Rd Chandler AZ',
+    webKey: '894570897696176'
+}
+
 test('test usStreetAPI', () => {
     return sgGeocode(obj1).then(res => {
         expect(res.lat).toBe(33.32371)
@@ -40,5 +45,11 @@ test('test invalid zip code', () => {
 test('test non numeric zip code', () => {
     return sgGeocode(obj4).then(res => {
         expect(res.error).toBe("Blank lookup (you must provide a ZIP Code and/or City/State combination).")
+    })
+});
+
+test('test invalid key', () => {
+    return sgGeocode(obj5).then(res => {
+        expect(res.error).toBe("Unauthorized: The credentials were provided incorrectly or did not match any existing active credentials.")
     })
 });
