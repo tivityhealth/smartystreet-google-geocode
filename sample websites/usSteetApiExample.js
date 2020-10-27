b496eacee6fc03a7f51d2cef15a36fda7ffc9841
vishaltvty <Vishal.Chilka@tivityhealth.com>
2020-10-27 13:32:46 -0700

    Added sample examples

---
+++ sample websites/usSteetApiExample.js
@@ -0,0 +1,24 @@
+import {sgGeocode} from '../sgGeocode';
+
+//passing address to get lat long
+    //creating address object
+    let addrObject = {
+        address: '1050 S Terrace Rd Tempe AZ'
+    }
+
+    //calling the library with zip code
+    sgGeocode(addrObject).then((res) => {
+        //response returns lat long object
+        console.log('Result', res)
+    })
+
+//When smartystreet fails to return lat long, the call is redirected to google geocoder
+    //eg: 1448 S Spectrum Blvd Chandler AZ
+    let businessObject = {
+        address: '1448 S Spectrum Blvd Chandler AZ'
+    }
+
+    sgGeocode(businessObject).then((res) => {
+        //response returns lat long object
+        console.log('Result', res)
+    })
---
+++ sample websites/zipApiExample.js
@@ -0,0 +1,27 @@
+import {sgGeocode} from '../sgGeocode';
+
+//passing zipcode to get lat long
+    //creating zipcode object
+    let zipObject = {
+        zipcode: '85225'
+    }
+
+    //calling the library with zip code
+    sgGeocode(zipObject).then((res) => {
+        //response returns lat long object
+        console.log('Result', res)
+    })
+
+//passing City/State to get lat long
+    //creating city/state object
+    let cityStateObject = {
+        city: 'Chandler',
+        state: 'AZ'
+    }
+
+    //calling the library with zip code
+    sgGeocode(cityStateObject).then((res) => {
+        //response returns lat long object
+        console.log('Result', res)
+    })
+
