891f9733dfcab61976b7f2f0dc73eacbfaf48761
vishaltvty <Vishal.Chilka@tivityhealth.com>
2020-12-16 15:00:28 -0700

    Added comments

--- npm/src/sgGeocode.js
+++ npm/src/sgGeocode.js
@@ -1,10 +1,8 @@
 const {callZipApi} = require("./zipApi")
 const { callUSStreetApi } = require("./usStreetApi");
 
-//user passes prediction
-//Google autocomplete as input
-
 const sgGeocode = (options) => {
+  //When user passes either zipcode or city-state combo
   if (options.zipcode || (options.city && options.state)) {
     let zipData = {
       city: options.city,
@@ -14,12 +12,14 @@ const sgGeocode = (options) => {
     };
     return callZipApi(zipData)
         .then(({result}) => result)
-  } else if (options.address) {
-      //undefined
+  }
+  //When the user passes complete/partial address
+  else if (options.address) {
       return callUSStreetApi(options)
         .then(({result}) =>  result )
-  } else if (options.prediction) {
-    console.log(123);
+  }
+  //When the user passes Google autocomplete object as input
+  else if (options.prediction) {
      return usePrediction(options)
         .then(({result}) =>  result )
   }
@@ -29,6 +29,7 @@ const usePrediction = (options) => {
 
   let zipKeyWords = ["postal_code", "locality"];
 
+  //Check if autocomplete object is zipcode compatible  
   if(zipKeyWords.some(x => options.prediction.types.includes(x))){
     let zipData = {
       city: '',
@@ -38,16 +39,19 @@ const usePrediction = (options) => {
     };
     let termsLength = options.prediction.terms.length - 1;
 
+    //Checks if it has zipcode specifically
     if(!isNaN(options.prediction.terms[termsLength - 1].value)){
       zipData.zipcode = options.prediction.terms[termsLength - 1].value;
     }
+    //Checks is autocomplete has only City-State combo
     else if(termsLength == 2){
       zipData.city = options.prediction.terms[0].value;
       zipData.state = options.prediction.terms[1].value;
     }
-
     return callZipApi(zipData)
-  } else {
+  } 
+  //If no zipcode, then call SmartyStreets USStreet end point
+  else {
     let streetData = {
       address: options.prediction.description,
       webKey: options.webKey
@@ -58,43 +62,3 @@ const usePrediction = (options) => {
 }
 
 module.exports.sgGeocode = sgGeocode;
-
-
-
-
-
-//prediction[0]
-
-//options.prediction = prdiction[0]
-
-/**
-let options = {
-         "description" : "155 East Warner Road, Gilbert, AZ, USA",         
-         "place_id" : "ChIJDfaWdmepK4cRJ-IVtbEzX1o",
-         "reference" : "ChIJDfaWdmepK4cRJ-IVtbEzX1o",
-         "structured_formatting" :
-         "terms" : [
-            {
-               "offset" : 0,
-               "value" : "155"
-            },
-            {
-               "offset" : 4,
-               "value" : "East Warner Road"
-            },
-            {
-               "offset" : 22,
-               "value" : "Gilbert"
-            },
-            {
-               "offset" : 31,
-               "value" : "AZ"
-            },
-            {
-               "offset" : 35,
-               "value" : "USA"
-            }
-         ],
-         "types" : [ "premise", "geocode" ]
-      },
-      */
