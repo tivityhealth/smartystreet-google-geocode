6ec125983d1a34da224725372ee8cfd9f0dbc3e0
vishaltvty <Vishal.Chilka@tivityhealth.com>
2020-12-16 14:53:34 -0700

    Added support Google Autocomplete object as input

--- npm/src/sgGeocode.js
+++ npm/src/sgGeocode.js
@@ -1,6 +1,9 @@
 const {callZipApi} = require("./zipApi")
 const { callUSStreetApi } = require("./usStreetApi");
 
+//user passes prediction
+//Google autocomplete as input
+
 const sgGeocode = (options) => {
   if (options.zipcode || (options.city && options.state)) {
     let zipData = {
@@ -15,7 +18,83 @@ const sgGeocode = (options) => {
       //undefined
       return callUSStreetApi(options)
         .then(({result}) =>  result )
+  } else if (options.prediction) {
+    console.log(123);
+     return usePrediction(options)
+        .then(({result}) =>  result )
   }
 };
 
+const usePrediction = (options) => {
+
+  let zipKeyWords = ["postal_code", "locality"];
+
+  if(zipKeyWords.some(x => options.prediction.types.includes(x))){
+    let zipData = {
+      city: '',
+      state: '',
+      zipcode: '',
+      webKey: options.webKey
+    };
+    let termsLength = options.prediction.terms.length - 1;
+
+    if(!isNaN(options.prediction.terms[termsLength - 1].value)){
+      zipData.zipcode = options.prediction.terms[termsLength - 1].value;
+    }
+    else if(termsLength == 2){
+      zipData.city = options.prediction.terms[0].value;
+      zipData.state = options.prediction.terms[1].value;
+    }
+
+    return callZipApi(zipData)
+  } else {
+    let streetData = {
+      address: options.prediction.description,
+      webKey: options.webKey
+    }
+
+    return callUSStreetApi(streetData)
+  }
+}
+
 module.exports.sgGeocode = sgGeocode;
+
+
+
+
+
+//prediction[0]
+
+//options.prediction = prdiction[0]
+
+/**
+let options = {
+         "description" : "155 East Warner Road, Gilbert, AZ, USA",         
+         "place_id" : "ChIJDfaWdmepK4cRJ-IVtbEzX1o",
+         "reference" : "ChIJDfaWdmepK4cRJ-IVtbEzX1o",
+         "structured_formatting" :
+         "terms" : [
+            {
+               "offset" : 0,
+               "value" : "155"
+            },
+            {
+               "offset" : 4,
+               "value" : "East Warner Road"
+            },
+            {
+               "offset" : 22,
+               "value" : "Gilbert"
+            },
+            {
+               "offset" : 31,
+               "value" : "AZ"
+            },
+            {
+               "offset" : 35,
+               "value" : "USA"
+            }
+         ],
+         "types" : [ "premise", "geocode" ]
+      },
+      */
--- npm/test/latlon.test.js
+++ npm/test/latlon.test.js
@@ -22,6 +22,90 @@ let obj5 = {
     webKey: '894570897696176'
 }
 
+let obj6 = {
+    prediction: {
+        "description" : "Chandler, AZ 85225, USA",
+        "matched_substrings" : [
+           {
+              "length" : 5,
+              "offset" : 13
+           }
+        ],
+        "terms" : [
+           {
+              "offset" : 0,
+              "value" : "Chandler"
+           },
+           {
+              "offset" : 10,
+              "value" : "AZ"
+           },
+           {
+              "offset" : 13,
+              "value" : "85225"
+           },
+           {
+              "offset" : 20,
+              "value" : "USA"
+           }
+        ],
+        "types" : [ "postal_code", "geocode" ]
+     },
+     webKey: '8945708976961762' 
+}
+
+let obj7 = {
+    prediction: {
+        "description" : "Tempe, AZ, USA",
+        "terms" : [
+           {
+              "offset" : 0,
+              "value" : "Tempe"
+           },
+           {
+              "offset" : 7,
+              "value" : "AZ"
+           },
+           {
+              "offset" : 11,
+              "value" : "USA"
+           }
+        ],
+        "types" : [ "locality", "political", "geocode" ]
+     },
+     webKey: '8945708976961762' 
+}
+
+let obj8 = {
+    prediction: {
+        "description" : "155 East Frye Road, Chandler, AZ, USA",
+        "terms" : [
+           {
+              "offset" : 0,
+              "value" : "155"
+           },
+           {
+              "offset" : 4,
+              "value" : "East Frye Road"
+           },
+           {
+              "offset" : 20,
+              "value" : "Chandler"
+           },
+           {
+              "offset" : 30,
+              "value" : "AZ"
+           },
+           {
+              "offset" : 34,
+              "value" : "USA"
+           }
+        ],
+        "types" : [ "premise", "geocode" ]
+     },
+     webKey: '8945708976961762' 
+}
+
 test('test usStreetAPI', () => {
     return sgGeocode(obj1).then(res => {
         expect(res.lat).toBe(33.32371)
@@ -51,5 +135,26 @@ test('test non numeric zip code', () => {
 test('test invalid key', () => {
     return sgGeocode(obj5).then(res => {
         expect(res.error).toBe("Unauthorized: The credentials were provided incorrectly or did not match any existing active credentials.")
+    })
+});
+
+test('test Prediction Zipcode', () => {
+    return sgGeocode(obj6).then(res => {
+        expect(res.lat).toBe(33.31666)
+        expect(res.lng).toBe(-111.83182)
+    })
+});
+
+test('test Prediction CityState', () => {
+    return sgGeocode(obj7).then(res => {
+        expect(res.lat).toBe(33.43681)
+        expect(res.lng).toBe(-111.943)
+    })
+});
+
+test('test Prediction usStreetAPI', () => {
+    return sgGeocode(obj8).then(res => {
+        expect(res.lat).toBe(33.32371)
+        expect(res.lng).toBe(-111.83018)
     })
 });
