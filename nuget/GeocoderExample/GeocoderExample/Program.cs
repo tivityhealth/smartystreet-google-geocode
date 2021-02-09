43ed72723397f4fb5b5ad8c4404c2b2a483e9082
vishaltvty <Vishal.Chilka@tivityhealth.com>
2021-02-09 08:43:38 -0700

    Added nuget example

---
+++ nuget/GeocoderExample/GeocoderExample/Program.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+using SmartyStreetsGoogleGeocode;
+using System;
+
+namespace GeocoderExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GeocodeInput geocodeInput = new GeocodeInput("85225", null, null);
+            GeoPoint gp = new GeoPoint();
+
+            CreateHostBuilder(args).Build().Run();
+
+            gp = SgGeocoder.CallSgGeocoder(geocodeInput);
+
+            Console.WriteLine(gp.ToString());
+
+        }
+
+        static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
