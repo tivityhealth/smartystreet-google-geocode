578275f3469ad728a12180ce68f6541f87a27147
vishaltvty <Vishal.Chilka@tivityhealth.com>
2021-02-10 08:50:54 -0700

    Removed comments

--- nuget/SmartyStreetsGoogleGeocode/SmartyStreetsGoogleGeocode/UsStreetApi.cs
+++ nuget/SmartyStreetsGoogleGeocode/SmartyStreetsGoogleGeocode/UsStreetApi.cs
@@ -7,59 +7,58 @@ namespace SmartyStreetsGoogleGeocode
 {
     class UsStreetApi
     {
-		public static GeoPoint CallUsStreet(GeocodeInput options)
-		{
-			var authId = Runtime.SmartyStreetsAuthId;
-			var authToken = Runtime.SmartyStreetsAuthToken;
+        public static GeoPoint CallUsStreet(GeocodeInput options)
+        {
+            var authId = Runtime.SmartyStreetsAuthId;
+            var authToken = Runtime.SmartyStreetsAuthToken;
 
-			var client = new ClientBuilder(authId, authToken)
-				//.WithCustomBaseUrl("us-street.api.smartystreets.com")
-				//.ViaProxy("http://localhost:8080", "username", "password") // uncomment this line to point to the specified proxy.
-				.BuildUsStreetApiClient();
+            var client = new ClientBuilder(authId, authToken)
+                //.WithCustomBaseUrl("us-street.api.smartystreets.com")
+                //.ViaProxy("http://localhost:8080", "username", "password") // uncomment this line to point to the specified proxy.
+                .BuildUsStreetApiClient();
 
-			// Documentation for input fields can be found at:
-			// https://smartystreets.com/docs/us-street-api#input-fields
+            // Documentation for input fields can be found at:
+            // https://smartystreets.com/docs/us-street-api#input-fields
 
-			var lookup = new Lookup(options.address)
-			{
-				MaxCandidates = 1,
-				MatchStrategy = Lookup.INVALID // "invalid" is the most permissive match,
-											   // this will always return at least one result even if the address is invalid.
-											   // Refer to the documentation for additional MatchStrategy options.
-			};
+            var lookup = new Lookup(options.address)
+            {
+                MaxCandidates = 1,
+                MatchStrategy = Lookup.INVALID // "invalid" is the most permissive match,
+                                               // this will always return at least one result even if the address is invalid.
+                                               // Refer to the documentation for additional MatchStrategy options.
+            };
 
-			try
-			{
-				client.Send(lookup);
-			}
-			catch (SmartyException ex)
-			{
-				Console.WriteLine(ex.Message);
-				return null;
-			}
-			catch (IOException ex)
-			{
-				Console.WriteLine(ex.Message);
-				return null;
-			}
-			catch (Exception ex)
-			{
-				throw ex;
-			}
+            try
+            {
+                client.Send(lookup);
+            }
+            catch (SmartyException ex)
+            {
+                Console.WriteLine(ex.Message);
+                return null;
+            }
+            catch (IOException ex)
+            {
+                Console.WriteLine(ex.Message);
+                return null;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
 
-			var candidates = lookup.Result;
+            var candidates = lookup.Result;
 
-			if (candidates.Count == 0)
-			{
-				Console.WriteLine("SmartyStreets unable to lookup this address. Calling Google geocoder");
-				return null;
-			}
+            if (candidates.Count == 0)
+            {
+                return null;
+            }
 
-			var firstCandidate = candidates[0];
+            var firstCandidate = candidates[0];
 
-			GeoPoint geoPoint = new GeoPoint(firstCandidate.Metadata.Latitude, firstCandidate.Metadata.Longitude);
+            GeoPoint geoPoint = new GeoPoint(firstCandidate.Metadata.Latitude, firstCandidate.Metadata.Longitude);
 
-			return geoPoint;
-		}
-	}
+            return geoPoint;
+        }
+    }
 }
--- nuget/SmartyStreetsGoogleGeocode/SmartyStreetsGoogleGeocode/ZipApi.cs
+++ nuget/SmartyStreetsGoogleGeocode/SmartyStreetsGoogleGeocode/ZipApi.cs
@@ -5,66 +5,65 @@ using SmartyStreets.USZipCodeApi;
 
 namespace SmartyStreetsGoogleGeocode
 {
-	class ZipApi
-	{
-		public static GeoPoint CallZip(GeocodeInput options)
-		{
-			// You don't have to store your keys in environment variables, but we recommend it.
-			var authId = Runtime.SmartyStreetsAuthId;
-			var authToken = Runtime.SmartyStreetsAuthToken;
-			var client = new ClientBuilder(authId, authToken).BuildUsZipCodeApiClient();
+    class ZipApi
+    {
+        public static GeoPoint CallZip(GeocodeInput options)
+        {
+            // You don't have to store your keys in environment variables, but we recommend it.
+            var authId = Runtime.SmartyStreetsAuthId;
+            var authToken = Runtime.SmartyStreetsAuthToken;
+            var client = new ClientBuilder(authId, authToken).BuildUsZipCodeApiClient();
 
-			// Documentation for input fields can be found at:
-			// https://smartystreets.com/docs/us-zipcode-api#input-fields
+            // Documentation for input fields can be found at:
+            // https://smartystreets.com/docs/us-zipcode-api#input-fields
 
-			var lookup = new Lookup
-			{
-				City = options.city,
-				State = options.state,
-				ZipCode = options.zipcode,
-			};
+            var lookup = new Lookup
+            {
+                City = options.city,
+                State = options.state,
+                ZipCode = options.zipcode,
+            };
 
-			try
-			{
-				client.Send(lookup);
-			}
-			catch (SmartyException ex)
-			{
-				throw ex;
-			}
-			catch (IOException ex)
-			{
-				throw ex;
-			}
+            try
+            {
+                client.Send(lookup);
+            }
+            catch (SmartyException ex)
+            {
+                throw ex;
+            }
+            catch (IOException ex)
+            {
+                throw ex;
+            }
 
-			var result = lookup.Result;
+            var result = lookup.Result;
 
-			if (result.Status == "invalid_zipcode" || result.Status == "invalid_state" || result.Status == "invalid_city")
-			{
-				throw new ApplicationException("Invalid Zip or State or City");
-			}
-			else if (result.Status == "conflict")
-			{
-				throw new ApplicationException("Conflicting ZIP Code/City/State information.");
-			}
-			else if(result.Status == "blank")
-			{
-				throw new ApplicationException("You must provide a ZIP Code and/or City/State combination. Calling Google Geocoder");				
-			}
+            if (result.Status == "invalid_zipcode" || result.Status == "invalid_state" || result.Status == "invalid_city")
+            {
+                throw new ApplicationException("Invalid Zip or State or City");
+            }
+            else if (result.Status == "conflict")
+            {
+                throw new ApplicationException("Conflicting ZIP Code/City/State information.");
+            }
+            else if (result.Status == "blank")
+            {
+                throw new ApplicationException("You must provide a ZIP Code and/or City/State combination. Calling Google Geocoder");
+            }
 
-			var zipCodes = result.ZipCodes[0];
+            var zipCodes = result.ZipCodes[0];
 
-			if(zipCodes == null)
-			{
-				Console.WriteLine("SmartyStreets unable to lookup this address. Calling Google geocoder");
-				return null;
-			}
+            if (zipCodes == null)
+            {
+                return null;
+            }
 
-			GeoPoint geoPoint = new GeoPoint();
-			geoPoint.Latitude = zipCodes.Latitude;
-			geoPoint.Longitude = zipCodes.Longitude;
+            GeoPoint geoPoint = new GeoPoint();
+            geoPoint.Latitude = zipCodes.Latitude;
+            geoPoint.Longitude = zipCodes.Longitude;
 
-			return geoPoint;
-		}
-	}
+            return geoPoint;
+        }
+    }
 }
