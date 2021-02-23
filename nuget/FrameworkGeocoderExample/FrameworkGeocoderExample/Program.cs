b6fde7fbd184aef993735f0e7db3c24030b9e04a
vishaltvty <Vishal.Chilka@tivityhealth.com>
2021-02-23 01:18:40 -0700

    Added FrameworkGeocoder example

---
+++ nuget/FrameworkGeocoderExample/FrameworkGeocoderExample/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Configuration;
+using SmartyStreetsGoogleGeocode;
+
+namespace FrameworkGeocoderExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GeocodeInput geocodeInput = new GeocodeInput("85225", null, null);
+
+            SgGeocoder sgGeocoder = new SgGeocoder(new AuthOptions
+            {
+                SmartyStreetsAuthId = ConfigurationManager.AppSettings.Get("SmartyStreets_AuthId"),
+                SmartyStreetsAuthToken = ConfigurationManager.AppSettings.Get("SmartyStreets_AuthToken"),
+                GoogleApiKey = ConfigurationManager.AppSettings.Get("Google_Api_Key"),
+            });
+            GeoPoint result = sgGeocoder.GetLatLng(geocodeInput);
+
+            Console.WriteLine(result);
+        }
+    }
+}
