0e292f15d0d7e20f436be97e613d0bd5bfe983df
vishaltvty <Vishal.Chilka@tivityhealth.com>
2021-03-22 13:47:57 -0700

    Unit test updated

--- nuget/SmartyStreetsGoogleGeocode/XUnitTestSmartyStreetsGoogleGeocode/UnitTest1.cs
+++ nuget/SmartyStreetsGoogleGeocode/XUnitTestSmartyStreetsGoogleGeocode/UnitTest1.cs
@@ -6,16 +6,18 @@ namespace XUnitTestSmartyStreetsGoogleGeocode
 {
     public class UnitTest1
     {
+        SgGeocoder sggeocoder = new SgGeocoder(new AuthOptions
+        {
+            SmartyStreetsAuthId = Environment.GetEnvironmentVariable("SmartyStreets_AuthId"),
+            SmartyStreetsAuthToken = Environment.GetEnvironmentVariable("SmartyStreets_AuthToken"),
+            GoogleApiKey = Environment.GetEnvironmentVariable("Google_Api_Key")
+        });
+
         [Fact]
         public void TestZipApiWithZipCodeReturnsResult()
         {
             string zip = "85225";
             GeocodeInput dObj = new GeocodeInput(zip, null, null);
-            SgGeocoder sggeocoder = new SgGeocoder(new AuthOptions { 
-                SmartyStreetsAuthId = Environment.GetEnvironmentVariable("SmartyStreets_AuthId"),
-                SmartyStreetsAuthToken = Environment.GetEnvironmentVariable("SmartyStreets_AuthToken"),
-                GoogleApiKey = Environment.GetEnvironmentVariable("Google_Api_Key")
-            });
             GeoPoint gp = sggeocoder.GetLatLng(dObj);
 
             Assert.Equal("(33.31666" + "\u00B0" + "N,-111.83182" + "\u00B0" + "E)", gp.ToString());
@@ -27,13 +29,6 @@ namespace XUnitTestSmartyStreetsGoogleGeocode
             //LaunchSettingsFixture.SetEnvVariable();
             string address = "155 E Frye Rd Chandler AZ";
             GeocodeInput dObj = new GeocodeInput(address);
-
-            SgGeocoder sggeocoder = new SgGeocoder(new AuthOptions
-            {
-                SmartyStreetsAuthId = Environment.GetEnvironmentVariable("SmartyStreets_AuthId"),
-                SmartyStreetsAuthToken = Environment.GetEnvironmentVariable("SmartyStreets_AuthToken"),
-                GoogleApiKey = Environment.GetEnvironmentVariable("Google_Api_Key")
-            });
             GeoPoint gp = sggeocoder.GetLatLng(dObj);
 
             Assert.Equal("(33.32371" + "\u00B0" + "N,-111.83018" + "\u00B0" + "E)", gp.ToString());
@@ -45,13 +40,6 @@ namespace XUnitTestSmartyStreetsGoogleGeocode
             //LaunchSettingsFixture.SetEnvVariable();
             string add = "LA Fitness, Arizona Ave Chandler AZ";
             GeocodeInput dObj = new GeocodeInput(add);
-
-            SgGeocoder sggeocoder = new SgGeocoder(new AuthOptions
-            {
-                SmartyStreetsAuthId = Environment.GetEnvironmentVariable("SmartyStreets_AuthId"),
-                SmartyStreetsAuthToken = Environment.GetEnvironmentVariable("SmartyStreets_AuthToken"),
-                GoogleApiKey = Environment.GetEnvironmentVariable("Google_Api_Key")
-            });
             GeoPoint gp = sggeocoder.GetLatLng(dObj);
 
             Assert.Equal("(33.248528" + "\u00B0" + "N,-111.8381307" + "\u00B0" + "E)", gp.ToString());
@@ -63,12 +51,6 @@ namespace XUnitTestSmartyStreetsGoogleGeocode
             //LaunchSettingsFixture.SetEnvVariable();
             string zip = "abcde";
             GeocodeInput dObj = new GeocodeInput(zip, null, null);
-            SgGeocoder sggeocoder = new SgGeocoder(new AuthOptions
-            {
-                SmartyStreetsAuthId = Environment.GetEnvironmentVariable("SmartyStreets_AuthId"),
-                SmartyStreetsAuthToken = Environment.GetEnvironmentVariable("SmartyStreets_AuthToken"),
-                GoogleApiKey = Environment.GetEnvironmentVariable("Google_Api_Key")
-            });
             Exception ex = Assert.Throws<ApplicationException>(() => sggeocoder.GetLatLng(dObj));
 
             Assert.Equal("You must provide a ZIP Code and/or City/State combination. Calling Google Geocoder", ex.Message);
@@ -81,14 +63,6 @@ namespace XUnitTestSmartyStreetsGoogleGeocode
             string city = "Chandler";
             string state = "AZ";
             GeocodeInput dObj = new GeocodeInput(null, city, state);
-
-            SgGeocoder sggeocoder = new SgGeocoder(new AuthOptions
-            {
-                SmartyStreetsAuthId = Environment.GetEnvironmentVariable("SmartyStreets_AuthId"),
-                SmartyStreetsAuthToken = Environment.GetEnvironmentVariable("SmartyStreets_AuthToken"),
-                GoogleApiKey = Environment.GetEnvironmentVariable("Google_Api_Key")
-            });
-
             GeoPoint gp = sggeocoder.GetLatLng(dObj);
 
             Assert.Equal("(33.32212" + "\u00B0" + "N,-111.87374" + "\u00B0" + "E)", gp.ToString());
@@ -100,13 +74,6 @@ namespace XUnitTestSmartyStreetsGoogleGeocode
             //LaunchSettingsFixture.SetEnvVariable();
             string state = "AZ";
             GeocodeInput dObj = new GeocodeInput(null, null, state);
-
-            SgGeocoder sggeocoder = new SgGeocoder(new AuthOptions
-            {
-                SmartyStreetsAuthId = Environment.GetEnvironmentVariable("SmartyStreets_AuthId"),
-                SmartyStreetsAuthToken = Environment.GetEnvironmentVariable("SmartyStreets_AuthToken"),
-                GoogleApiKey = Environment.GetEnvironmentVariable("Google_Api_Key")
-            });
             Exception ex = Assert.Throws<ArgumentNullException>(() => sggeocoder.GetLatLng(dObj));
 
             Assert.Equal("City/State cannot be null or empty (Parameter 'CityState')", ex.Message);
@@ -118,13 +85,6 @@ namespace XUnitTestSmartyStreetsGoogleGeocode
             //LaunchSettingsFixture.SetEnvVariable();
             string city = "Chandler";
             GeocodeInput dObj = new GeocodeInput(null, city, null);
-
-            SgGeocoder sggeocoder = new SgGeocoder(new AuthOptions
-            {
-                SmartyStreetsAuthId = Environment.GetEnvironmentVariable("SmartyStreets_AuthId"),
-                SmartyStreetsAuthToken = Environment.GetEnvironmentVariable("SmartyStreets_AuthToken"),
-                GoogleApiKey = Environment.GetEnvironmentVariable("Google_Api_Key")
-            });
             Exception ex = Assert.Throws<ArgumentNullException>(() => sggeocoder.GetLatLng(dObj));
 
             Assert.Equal("City/State cannot be null or empty (Parameter 'CityState')", ex.Message);
@@ -134,13 +94,6 @@ namespace XUnitTestSmartyStreetsGoogleGeocode
         public void TestNullArguments()
         {
             GeocodeInput dObj = new GeocodeInput(null, null, null);
-
-            SgGeocoder sggeocoder = new SgGeocoder(new AuthOptions
-            {
-                SmartyStreetsAuthId = Environment.GetEnvironmentVariable("SmartyStreets_AuthId"),
-                SmartyStreetsAuthToken = Environment.GetEnvironmentVariable("SmartyStreets_AuthToken"),
-                GoogleApiKey = Environment.GetEnvironmentVariable("Google_Api_Key")
-            });
             Exception ex = Assert.Throws<ArgumentNullException>(() => sggeocoder.GetLatLng(dObj));
 
             Assert.Equal("Arguments cannot be null or empty (Parameter 'GeocodeInput')", ex.Message);
@@ -150,13 +103,6 @@ namespace XUnitTestSmartyStreetsGoogleGeocode
         public void TestEmptyArguments()
         {
             GeocodeInput dObj = new GeocodeInput("");
-
-            SgGeocoder sggeocoder = new SgGeocoder(new AuthOptions
-            {
-                SmartyStreetsAuthId = Environment.GetEnvironmentVariable("SmartyStreets_AuthId"),
-                SmartyStreetsAuthToken = Environment.GetEnvironmentVariable("SmartyStreets_AuthToken"),
-                GoogleApiKey = Environment.GetEnvironmentVariable("Google_Api_Key")
-            });
             Exception ex = Assert.Throws<ArgumentNullException>(() => sggeocoder.GetLatLng(dObj));
 
             Assert.Equal("Arguments cannot be null or empty (Parameter 'GeocodeInput')", ex.Message);
